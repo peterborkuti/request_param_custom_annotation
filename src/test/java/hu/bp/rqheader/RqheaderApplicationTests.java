a827538b854c1da99e8e429346cb28b8ca0f1c8f
Peter Borkuti <peter-zsolt.borkuti@ext.ec.europa.eu>
2023-11-03 13:28:46 +0100

    Spring initializr

---
+++ src/test/java/hu/bp/rqheader/RqheaderApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.bp.rqheader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RqheaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
